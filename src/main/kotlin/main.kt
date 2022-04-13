3ae2193d90fb1fbd49f17523801d76bc2c213e57
Коваль Вероника <veronika99k@yandex.ru>
2022-04-13 22:04:34 +0300

    hours

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,14 +5,13 @@ fun main() {
     val hours = minutes / 60
 
     print(agoToText(time, minutes, hours))
-
 }
 
-fun agoToText(time:Int, minutes:Int, minutesHours:Int): String {
+fun agoToText(time:Int, minutes:Int, hours:Int): String {
     val converterTime = when(time){
         in 0..60 -> "Была на сайте только что"
         in 61..(60*60) -> "Была в сети $minutes ${secondsToMinutes(minutes)}"
-        in (60 * 60 + 1)..(24 * 60 * 60) -> "Была в сети $minutesHours ${minutesToHours(minutesHours)}"
+        in (60 * 60 + 1)..(24 * 60 * 60) -> "Была в сети $hours ${minutesToHours(hours)}"
         in (24 * 60 * 60)..((24 * 60 * 60)*2) -> "Была в сети сегодн"
         in ((24 * 60 * 60)*2)..((24 * 60 * 60)*3) -> "Была в сети вчера"
         in ((24 * 60 * 60)*3)..((24 * 60 * 60)*7) -> "Была в сети давно"
@@ -27,8 +26,8 @@ fun secondsToMinutes(minutes: Int):String{
     else "минут назад"
 }
 
-fun minutesToHours(minutesHours:Int):String{
-    return if(minutesHours % 10 == 1 && minutesHours % 100 != 11) "час назад"
-    else if(minutesHours %10 == 2 || minutesHours % 10 == 3 || minutesHours % 10 == 4) "часа назад"
+fun minutesToHours(hours:Int):String{
+    return if(hours % 10 == 1 && hours % 100 != 11) "час назад"
+    else if(hours %10 == 2 || hours % 10 == 3 || hours % 10 == 4) "часа назад"
     else "часов назад"
 }
